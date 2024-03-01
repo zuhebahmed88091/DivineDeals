c009671ad70faefb09e668e96879f067fc66681b
shihabrafio <aimshihabuddinkhan@gmail.com>
2024-03-02 01:50:03 +0600

    Update cart icon and menu styling

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,6 @@
 import "./Navbar.css";
 import logo from "../../assets/xnxx.png";
-import cart_icon from "../../assets/cart_icon.png";
+import cart_icon from "../../assets/fart-cart.png";
 import { useState } from "react";
 
 export default function Navbar() {
@@ -20,7 +20,7 @@ export default function Navbar() {
         >
           Shop{" "}
           {menu === "shop" ? (
-            <hr className="w-4/5 h-1 rounded-lg" />
+            <hr className="w-12 h-1 rounded-lg bg-red-400 border-red-400" />
           ) : (
             <></>
           )}{" "}
@@ -33,7 +33,7 @@ export default function Navbar() {
         >
           Men{" "}
           {menu === "men" ? (
-            <hr className="w-4/5 h-1 rounded-lg" />
+            <hr className="w-10 h-1 rounded-lg bg-red-400 border-red-400" />
           ) : (
             <></>
           )}
@@ -46,7 +46,7 @@ export default function Navbar() {
         >
           Women{" "}
           {menu === "women" ? (
-            <hr className="w-4/5 h-1 rounded-lg" />
+            <hr className="w-20 h-1 rounded-lg bg-red-400 border-red-400" />
           ) : (
             <></>
           )}
@@ -59,17 +59,17 @@ export default function Navbar() {
         >
           Kids{" "}
           {menu === "kids" ? (
-            <hr className="w-4/5 h-1 rounded-lg" />
+            <hr className="w-12 h-1 rounded-lg bg-red-400 border-red-400" />
           ) : (
             <></>
           )}
         </li>
       </ul>
-      <div className="nav-login-cart flex items-center gap-x-16">
-        <button className="w-24 h-8 border text-xl border-gray-600 rounded-full text-gray-600  font-medium bg-white hover:bg-gray-200 focus:outline-none focus:border-gray-700 focus:ring focus:ring-gray-300">
+      <div className="nav-login-cart flex items-center gap-x-12">
+        <button className="w-24 h-8 border text-sm border-gray-600 rounded-full text-gray-600  font-medium bg-white hover:bg-gray-200 focus:outline-none focus:border-gray-700 focus:ring focus:ring-gray-300">
           Login
         </button>
-        <img src={cart_icon} alt="cart" />
+        <img src={cart_icon} className="w-12" alt="cart" />
         <div className="nav-cart-count w-4 h-4 flex justify-center items-center -mt-9 -ml-14 rounded-full text-xs bg-red-500 text-white">0</div>
       </div>
     </div>
