8baa89edd82efc1b0f9eb68de71b66455fc2d314
Zuheb Ahmed <zuhebahmed88091@gmail.com>
2024-03-02 01:04:42 +0600

    Added tailwind design

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,23 +4,73 @@ import cart_icon from "../../assets/cart_icon.png";
 import { useState } from "react";
 
 export default function Navbar() {
-    const [menu, setMenu] = useState("shop");
+  const [menu, setMenu] = useState("shop");
   return (
-    <div className="navbar">
-      <div className="nav-logo">
+    <div className="navbar flex justify-around p-4 shadow-md">
+      <div className="nav-logo flex items-center gap-2">
         <img src={logo} alt="logo" />
-        <p>DivineDeals</p>
+        <p className="text-gray-900 text-lg font-semibold">DivineDeals</p>
       </div>
-      <ul className="nav-menu">
-        <li onClick={() => {setMenu("shop")}}>Shop {menu === "shop" ? <hr /> : <></>} </li>
-        <li onClick={() => {setMenu("men")}}>Men {menu === "men" ? <hr /> : <></>}</li>
-        <li onClick={() => {setMenu("women")}}>Women {menu === "women" ? <hr /> : <></>}</li>
-        <li onClick={() => {setMenu("kids")}}>Kids {menu === "kids" ? <hr /> : <></>}</li>
+      <ul className="nav-menu flex items-center list-none gap-x-16 text-gray-600 text-base font-medium">
+        <li
+          className="flex flex-col items-center justify-center cursor-pointer"
+          onClick={() => {
+            setMenu("shop");
+          }}
+        >
+          Shop{" "}
+          {menu === "shop" ? (
+            <hr className="w-4/5 h-1 rounded-lg bg-red-500" />
+          ) : (
+            <></>
+          )}{" "}
+        </li>
+        <li
+          className="flex flex-col items-center justify-center cursor-pointer"
+          onClick={() => {
+            setMenu("men");
+          }}
+        >
+          Men{" "}
+          {menu === "men" ? (
+            <hr className="w-4/5 h-1 rounded-lg bg-red-500" />
+          ) : (
+            <></>
+          )}
+        </li>
+        <li
+          className="flex flex-col items-center justify-center cursor-pointer"
+          onClick={() => {
+            setMenu("women");
+          }}
+        >
+          Women{" "}
+          {menu === "women" ? (
+            <hr className="w-4/5 h-1 rounded-lg bg-red-500" />
+          ) : (
+            <></>
+          )}
+        </li>
+        <li
+          className="flex flex-col items-center justify-center cursor-pointer"
+          onClick={() => {
+            setMenu("kids");
+          }}
+        >
+          Kids{" "}
+          {menu === "kids" ? (
+            <hr className="w-4/5 h-1 rounded-lg bg-red-500" />
+          ) : (
+            <></>
+          )}
+        </li>
       </ul>
-      <div className="nav-login-cart">
-        <button>Login</button>
+      <div className="nav-login-cart flex items-center gap-x-16">
+        <button className="w-24 h-8 border border-gray-600 rounded-full text-gray-600 text-sm font-medium bg-white hover:bg-gray-200 focus:outline-none focus:border-gray-700 focus:ring focus:ring-gray-300">
+          Login
+        </button>
         <img src={cart_icon} alt="cart" />
-        <div className="nav-cart-count">0</div>
+        <div className="nav-cart-count w-4 h-4 flex justify-center items-center -mt-9 -ml-14 rounded-full text-xs bg-red-500 text-white">0</div>
       </div>
     </div>
   );
