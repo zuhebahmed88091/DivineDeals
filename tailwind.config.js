a14bdde73ccde6daf2219f349b675efc6d42118b
shihabrafio <aimshihabuddinkhan@gmail.com>
2024-03-05 01:22:56 +0600

    Add custom colors and background image to tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,13 @@ export default {
     "./src/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
-    extend: {},
+    extend: {colors: {
+      'custom-pink': '#fde1ff',
+      'custom-green': 'rgba(225, 255, 234, 0.133)', 
+    },
+    backgroundImage: {
+      'hero-gradient': 'linear-gradient(180deg, #fde1ff, rgba(225, 255, 234, 0.133) 66%)',
+    }},
   },
   plugins: [],
 }
