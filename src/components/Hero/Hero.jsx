27f752df4b39cc6c479c85bf4e762a7a3ddedbaf
shihabrafio <aimshihabuddinkhan@gmail.com>
2024-03-05 01:23:03 +0600

    Refactor Hero component styles

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -5,24 +5,24 @@ import hero_image from "../../assets/hero_image.png"
 
 export default function Hero() {
   return (
-    <div className="hero">
-      <div className="hero-left">
-        <h2>NEW ARRIVALS ONLY</h2>
+    <div className="h-screen bg-hero-gradient flex">
+      <div className="flex-1 flex flex-col justify-center pl-[10%] gap-3 leading-tight">
+        <h2 className="text-[#090909] text-3xl font-semibold">NEW ARRIVALS ONLY</h2>
         <div>
-          <div className="hero-hand-icon">
-            <p>new</p>
-            <img src={hand_icon} alt="hand-icon" />
+          <div className="flex items-center gap-6">
+            <p className="text-[#171717] text-[3rem] font-bold">new</p>
+            <img src={hand_icon} className="w-[15%]" alt="hand-icon" />
           </div>
-          <p>collections</p>
-          <p>for everyone</p>
+          <p className="text-[#171717] text-[3rem] font-bold">collections</p>
+          <p className="text-[#171717] text-[3rem] font-bold">for everyone</p>
         </div>
-        <div className="hero-latest-btn">
+        <div className="flex justify-center items-center gap-2 w-[19rem] h-[5rem] rounded-full mt-7 bg-[#ff4141] text-white text-[22px] font-medium">
             <div>Latest Collection</div>
             <img src={arrow_icon} alt="arrow" />
         </div>
       </div>
-      <div className="hero-right">
-        <img src={hero_image} alt="hero" />
+      <div className="flex-1 max-w-[30%] flex items-center justify-center">
+        <img src={hero_image} className="hidden md:block" alt="hero" />
       </div>
     </div>
   );
