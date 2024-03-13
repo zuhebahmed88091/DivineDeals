90f6a685ef6a8c45d29813f71db0a9954bbadffc
shihabrafio <aimshihabuddinkhan@gmail.com>
2024-03-13 17:37:17 +0600

    Add Popular component to Shop page

--- src/pages/Shop.jsx
+++ src/pages/Shop.jsx
@@ -1,9 +1,11 @@
 import Hero from "../components/Hero/Hero";
+import { Popular } from "../components/Popular/Popular";
 
 export default function Shop() {
   return (
     <div>
       <Hero />
+      <Popular />
     </div>
   )
 }
