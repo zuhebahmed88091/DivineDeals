3bfbae8d391791ac85bd063c47993a302574038f
shihabrafio <aimshihabuddinkhan@gmail.com>
2024-03-22 23:12:15 +0600

    Add Offers component with CSS styling

---
+++ src/components/Offers/Offers.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+import './Offers.css'
+import exclusive from '../../assets/exclusive_image.png'
+
+export const Offers = () => {
+return (
+    <div className='offers'>
+            <div className="offers-left">
+                    <h1>Exclusive</h1>
+                    <h1>Offers For You</h1>
+                    <p>ONLY ON BEST SELLERS PRODUCTS</p>
+                    <button>CHECK NOW</button>
+            </div>
+            <div className="offers-right">
+                    <img src={exclusive} alt="" />
+            </div>
+
+    </div>
+)
+}
