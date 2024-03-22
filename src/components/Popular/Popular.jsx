8466fe043df80816bf51beac0bba3d8e4d29dc83
shihabrafio <aimshihabuddinkhan@gmail.com>
2024-03-22 21:20:18 +0600

    Refactor Popular component layout and styles

--- src/components/Popular/Popular.jsx
+++ src/components/Popular/Popular.jsx
@@ -5,13 +5,13 @@ import { Item } from '../Item/Item'
 
 export const Popular = () => {
   return (
-    <div className='popular'>
-        <h1>POPULAR IN WOMEN</h1>
-        <hr/>
-        <div className="popular-item">
+    <div className='flex flex-col items-center gap-10 h-[90vh]'>
+        <h1 className="text-4xl text-gray-700 mt-10 font-semibold">POPULAR IN WOMEN</h1>
+        <hr className='w-[300px] h-[6px] rounded-lg bg-gray-900'/>
+        <div className="mt-12 flex gap-8">
             {data_product.map((item,i) => {
-                return <Item key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price} />
-            }
+                return <Item key={i} id={item.id} name={item.name} image={item.image} new_price={item.new_price} old_price={item.old_price} />;
+            })}
         </div>
     </div>
   )
