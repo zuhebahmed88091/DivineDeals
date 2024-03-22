3fa0ada803b8c3b2c05550fc02e1fbbac20c0f09
shihabrafio <aimshihabuddinkhan@gmail.com>
2024-03-22 21:20:14 +0600

    Refactor Item component to improve styling and readability

--- src/components/Item/Item.jsx
+++ src/components/Item/Item.jsx
@@ -3,18 +3,21 @@ import './Item.css'
 
 export const Item = (props) => {
   return (
-    <div className='item'>
-        <img src={props.image} alt="item" />
-        <p>{props.name}</p>
-        <div className='item-prices'>
-            <div className="item-price-new">
-                {props.new_price}
-            </div>
-            <div className="item-price-old">
-                {props.old_price}
-            </div>
+    <div className=' hover:scale-105 transition-transform duration-800'>
+      <div className='flex flex-row lg:flex-col'>
+        <div className='mx-8'>
+          <img src={props.image} alt="item" />
+          <p className='my-4 text-white'>{props.name}</p>
+          <div className='flex gap-5'>
+              <div className="text-gray-700 text-lg font-semibold">
+                  ${props.new_price}
+              </div>
+              <div className="text-gray-500 text-lg font-medium line-through">
+                  ${props.old_price}
+              </div>
+          </div>
         </div>
-
+      </div>
     </div>
   )
 }
